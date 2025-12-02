764250811453af02952c55d6a824ca42704a3462
Augusto-Otavio <thetavin21@gmail.com>
2025-12-02 12:31:33 -0300

    finishing css and starting js

---
+++ src/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+import "./libs/dayjs.js"
+
+import "./styles/appointments.css"
+import "./styles/button.css"
+import "./styles/global.css"
+import "./styles/header.css"
+import "./styles/utility.css"
+import "./styles/modal.css"
+
+import "./modules/form/modal.js"
---
+++ src/modules/form/modal.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("button-appoinment")
+const modal = document.getElementById("modal-appointment")
+
+btn.onclick = (e) => {
+  e.preventDefault()
+  
+  addcl
+}
