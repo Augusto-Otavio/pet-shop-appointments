b696c9b4bcb8f6a2be6191f4616d8b9f0ff51993
Augusto-Otavio <thetavin21@gmail.com>
2025-12-02 13:21:49 -0300

    finishing open and close modal

--- src/modules/form/modal.js
+++ src/modules/form/modal.js
@@ -1,8 +1,18 @@
 const btn = document.getElementById("button-appoinment")
 const modal = document.getElementById("modal-appointment")
+const blur = document.querySelector(".blur-background")
+const input = document.querySelector("input[type=text]")
+const cancel = document.getElementById("cancel")
 
 btn.onclick = (e) => {
   e.preventDefault()
-  
-  addcl
+  modal.classList.remove("none")
+  blur.classList.remove("none")
+  input.focus()
+}
+
+cancel.onclick = (e) => {
+  e.preventDefault()
+  modal.classList.add("none")
+  blur.classList.add("none")
 }
